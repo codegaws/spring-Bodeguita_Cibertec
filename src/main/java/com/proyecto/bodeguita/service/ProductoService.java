139af32e7d3d3c52cd833ef66031441124ca27aa
codegaws <codegaws@gmail.com>
2023-08-20 21:03:50 -0500

    crear el repository y las clases de servicios y el metodo show de ProductoController

---
+++ src/main/java/com/proyecto/bodeguita/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.proyecto.bodeguita.service;
+
+import java.util.Optional;
+
+import com.proyecto.bodeguita.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+	
+}
